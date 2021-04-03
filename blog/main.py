91e76c53fa63eae52203627e56cc5dc945411bfc
Nitish Saha <nitisaha@cisco.com>
2021-04-03 15:27:35 +0530

    Login API, authentication with jwt

--- blog/main.py
+++ blog/main.py
@@ -5,7 +5,7 @@ from database import engine, SessionLocal, get_db
 from sqlalchemy.orm import Session 
 from typing import List 
 from hashing import Hash
-from routers import blog, user
+from routers import blog, user, authentication
 
 
 app = FastAPI()
@@ -14,4 +14,4 @@ models.Base.metadata.create_all(engine)
 
 app.include_router(blog.router)
 app.include_router(user.router)
-
+app.include_router(authentication.router)
